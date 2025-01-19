46d996aa2d622c2369e17ad0ffddcd04a23e5b93
Alex Dou <alexdou@Alexs-MacBook-Pro.local>
2025-01-19 21:16:18 +0100

    Add feature to update user profiles

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from '@clerk/nextjs';
 export default function SignUpPage() {
   return (
     <main className="flex h-screen w-full items-center justify-center">
-      <SignUp />
+      <SignUp path="/sign-up" routing="path" signInUrl="/sign-in" unsafeMetadata={{ skipCaptcha: true }} />
     </main>
   );
 }
